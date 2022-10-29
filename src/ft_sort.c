b61a773e2d4fb01784aad55850502738e6765e13
Iker Sojo <ikersojo@hotmail.com>
2022-10-29 12:45:16 +0200

    update

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 15:55:08 by isojo-go          #+#    #+#             */
-/*   Updated: 2022/10/29 12:43:52 by isojo-go         ###   ########.fr       */
+/*   Updated: 2022/10/29 12:45:06 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,5 +132,10 @@ void	ft_sort(t_intlst **a, t_intlst **b, int n)
 			ft_sort_3(a);
 		else if (n == 4 || n == 5)
 			ft_sort_5(a, b, n);
+		else
+		{
+			ft_putstr_fd("too many numbers!\n", 1);
+			break ;
+		}
 	}
 }
